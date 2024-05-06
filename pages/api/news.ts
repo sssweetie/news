18606570cc12e8091ba8fc4762d495ca65fac181
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2024-05-06 21:42:31 +0300

    feat: ssr get news

---
+++ pages/api/news.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === 'GET') {
+    try {
+      const { offset, limit } = req.query;
+      const response = await axios.get(
+        `https://api.slingacademy.com/v1/sample-data/blog-posts?offset=${
+          offset ?? 1
+        }&limit=${limit ?? 10}`
+      );
+
+      return res.status(200).send(response.data);
+    } catch {
+      return res.status(400);
+    }
+  }
+}
